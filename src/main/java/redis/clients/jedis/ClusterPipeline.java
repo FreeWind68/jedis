33d732b11b5671de4bc87f2acb686d29e4991c12
qinfeng <qinfeng@300.cn>
2022-10-26 11:04:12 +0800

    init

---
+++ src/main/java/redis/clients/jedis/ClusterPipeline.java
@@ -0,0 +1,61 @@
+package redis.clients.jedis;
+
+import java.util.Set;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.providers.ClusterConnectionProvider;
+import redis.clients.jedis.util.IOUtils;
+
+public class ClusterPipeline extends MultiNodePipelineBase {
+
+  private final ClusterConnectionProvider provider;
+  private AutoCloseable closeable = null;
+
+  public ClusterPipeline(Set<HostAndPort> clusterNodes, JedisClientConfig clientConfig) {
+    this(new ClusterConnectionProvider(clusterNodes, clientConfig));
+    this.closeable = this.provider;
+  }
+
+  public ClusterPipeline(Set<HostAndPort> clusterNodes, JedisClientConfig clientConfig,
+      GenericObjectPoolConfig<Connection> poolConfig) {
+    this(new ClusterConnectionProvider(clusterNodes, clientConfig, poolConfig));
+    this.closeable = this.provider;
+  }
+
+  public ClusterPipeline(ClusterConnectionProvider provider) {
+    super(new ClusterCommandObjects());
+    this.provider = provider;
+  }
+
+  @Override
+  public void close() {
+    try {
+      super.close();
+    } finally {
+      IOUtils.closeQuietly(closeable);
+    }
+  }
+
+  @Override
+  protected HostAndPort getNodeKey(CommandArguments args) {
+    return provider.getNode(((ClusterCommandArguments) args).getCommandHashSlot());
+  }
+
+  @Override
+  protected Connection getConnection(HostAndPort nodeKey) {
+    return provider.getConnection(nodeKey);
+  }
+
+  /**
+   * This method must be called after constructor, if graph commands are going to be used.
+   */
+  public void prepareGraphCommands() {
+    super.prepareGraphCommands(provider);
+  }
+
+  /**
+   * TODO 防止Redis集群发生主从切换，所以在必要时刷新slot与master节点的映射关系
+   */
+  public void renewSlotCache(){
+    this.provider.renewSlotCache();
+  }
+}
